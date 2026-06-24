5ae0a2fa5076bc4eb788c214a657b3719d4d93f3
Miguel <miguelironsilva@gmail.com>
2026-06-24 10:40:21 -0300

    Feat: Criando js

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+//Define o objeto com os resultados(Banco de dados)
+const PlacarOficial ={
+    1:{time1:2, time2:1},
+    2:{time1:3, time2:0},
+}
+// Função salvar palpite
+function salvarPalpite(nomeJogo,id){
+
+// Capturar os elementos do DOM
+    const input1 = document.getElementById(`time1-${id}`);
+    const input2 = document.getElementById(`time2-${id}`);
+
+// Validação dos campos
+
+}
